867ff7bbbd4f0aa3de3a0140b6a468288cd34287
Neo Phukubye <neophukubye2001@gmail.com>
2026-08-03 18:49:37 +0200

    progress

--- forgepipeline_ai/__init__.py
+++ forgepipeline_ai/__init__.py
@@ -1,1 +1,24 @@
-"""Core components like the execution engine."""
+"""ForgePipeline AI - Agentic cloud deployment from intent to production."""
+
+from .analyzer import AnalysisResult, CodeAnalyzer
+from .dockerfile_generator import DockerfileGenerator
+from .execution_engine import ExecutionContext, ExecutionEngine, ExecutionError
+from .intent_parser import DeploymentIntent, IntentParser
+from .knowledge_base import KnowledgeBase
+from .planning_agent import DeploymentPlan, PlanningAgent, PlanStep, StepStatus
+
+__all__ = [
+    "AnalysisResult",
+    "CodeAnalyzer",
+    "DockerfileGenerator",
+    "ExecutionContext",
+    "ExecutionEngine",
+    "ExecutionError",
+    "DeploymentIntent",
+    "IntentParser",
+    "KnowledgeBase",
+    "DeploymentPlan",
+    "PlanningAgent",
+    "PlanStep",
+    "StepStatus",
+]
